3d7a96f551b33a63cd920b57355537bab6459f72
IsTimofeevEg <egortimofeevpv@gmail.com>
2024-02-02 23:23:23 +0300

    append push / pop functions

--- libs/data_structures/vector/voidVector.c
+++ libs/data_structures/vector/voidVector.c
@@ -1,9 +1,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
 #include "voidVector.h"
 
-//возвращает структуру-дескриптор вектор из n значений.
+
 vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
     vectorVoid result = {(int *) malloc(baseTypeSize * n), 0, n,
                          baseTypeSize};
@@ -14,7 +15,7 @@ vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
     return result;
 }
 
-//изменяет количество памяти, выделенное под хранение элементов вектора.
+
 void reserveV(vectorVoid *v, const size_t newCapacity) {
     if (newCapacity == 0)
         v->data = NULL;
@@ -30,17 +31,57 @@ void reserveV(vectorVoid *v, const size_t newCapacity) {
     v->capacity = newCapacity;
 }
 
-//удаляет элементы из контейнера, но не освобождает выделенную память.
+
 void shrinkToFitV(vectorVoid *v) {
     reserveV(v, v->size);
 }
 
-//освобождает память, выделенную поднеиспользуемые элементы.
+
 void clearV(vectorVoid *v) {
     v->size = 0;
 }
 
-//освобождает память, выделенную вектору.
+
 void deleteVectorV(vectorVoid *v) {
     free(v->data);
+}
+
+
+bool isEmptyV(vectorVoid *v) {
+    return v->size == 0;
+}
+
+
+bool isFullV(vectorVoid *v) {
+    return v->size == v->capacity;
+}
+
+
+void getVectorValueV(vectorVoid *v, size_t index, void *destination) {
+    char *source = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+}
+
+
+void setVectorValueV(vectorVoid *v, size_t index, void *source) {
+    char *destination = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+}
+
+
+void popBackV(vectorVoid *v) {
+    if (isEmptyV(v)) {
+        fprintf(stderr, "The vector is empty, we cannot fulfill your request");
+        exit(1);
+    }
+    v->size--;
+}
+
+
+void pushBackV(vectorVoid *v, void *source) {
+    if (v->capacity == 0)
+        reserveV(v, 1);
+    else if (v->size == v->capacity)
+        reserveV(v, v->size * 2);
+    setVectorValueV(v, v->size++, source);
 }
