82f4203391f42644728576b1a603e458acf3d5ec
IsTimofeevEg <egortimofeevpv@gmail.com>
2024-02-13 11:37:58 +0300

    add func strlen_

--- libs/string_/string_.c
+++ libs/string_/string_.c
@@ -1,3 +1,10 @@
-//
-// Created by User on 13.02.2024.
-//
+#include "string_.h"
+
+//возвращает количество символов в строке
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
+
--- libs/string_/string_.h
+++ libs/string_/string_.h
@@ -1,6 +1,10 @@
 #ifndef UNTITLED_STRING__H
 #define UNTITLED_STRING__H
 
+#include <stdlib.h>
+
+//возвращает количество символов в строке
+size_t strlen_(const char *begin);
 
 
 
